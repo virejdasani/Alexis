f769c71dd5eae8d567ac7ceb457e23e81862cb64
virejdasani <dasanivirej@gmail.com>
2020-10-17 17:52:32 +0530

    Add more commands

--- jarvis.py
+++ jarvis.py
@@ -1,13 +1,22 @@
+# TODO add colors in the Q&A's in terminal
 import datetime
+import random
 
 finished = False
 
 
-while finished == False:
+# Lists
+helloResponse = ["Hello to you too!", "Hey!", "Hola amigo", "Good to see you!"] 
+howAreYouResponse = ["Just doing my thing!", "I am great!", "Amazing!", "Feeling awesome!"]
 
+
+# MAIN LOOP
+while finished == False:
+    # Function to greet differently depending on the time of day
     def greet():
         # Get the current time 
         hour = datetime.datetime.now().hour
+
         # Check what part of the 24 hrs it is
 
         # 12am to 11:59am
@@ -31,10 +40,32 @@ while finished == False:
 
 
     # Add possible user commands here
-    if "hi" in command :
-        greet()
+    if "hi" in command or "hey" in command or "hello" in command:
+        print(random.choice(helloResponse))
+
+    elif "how are you" in command or "hows it going" in command or "how's it going" in command:
+        print(random.choice(howAreYouResponse))
+
+
+
+
     elif "bye" in command:
         print("Goodbye")
         finished = True    
+
     else:
-        print("I didn't get that")    
+        print("Sorry, I don't know that one")    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
