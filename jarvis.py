96dd1b5382d9f4e6ad7fb6aaac5c5aeb286b86e2
virejdasani <dasanivirej@gmail.com>
2020-10-17 23:13:51 +0530

    Add colors to the commands and responses

--- jarvis.py
+++ jarvis.py
@@ -16,30 +16,31 @@ def greet():
     # Get the current time 
     hour = datetime.datetime.now().hour
 
+    # "\033[1;32;40m" shows the string in green in the terminal
     # 12am to 11:59am
     if hour >= 0 and hour < 12 :
-        greeting = "Good Moning, "
+        greeting = "\033[1;32;40m\nGood Moning, "
 
     # 12pm to 5:59pm 
     if hour >= 12 and hour < 18:
-        greeting = "Good Afternoon, "
+        greeting = "\033[1;32;40m\nGood Afternoon, "
 
     # 6pm to 11:59pm 
     if hour >= 18  and hour != 0:
-        greeting = "Good Evening, "  
+        greeting = "\033[1;32;40m\nGood Evening, "  
     
     return greeting
 
+# This prints one of the above three greetings before taking user input
 print(greet())    
 
 # MAIN LOOP
 while finished == False:
 
-        
-
-
-    # Take input from user    
-    command = input("Enter Command: ").lower()
+    # This will prompt user for input in green color 
+    print("\033[1;32;40m\nEnter Command: ")
+    # Take input from user in yellow color   
+    command = input("\033[33m").lower()
 
 
     # Add possible user commands here
