5d711e9fa34908422ced3c4b8e78c2261519da21
RickyY1689 <Richard.jr.yang@gmail.com>
2022-12-04 15:40:07 +0000

    fixing broken tests

--- utils/tests/test_connect_four.py
+++ utils/tests/test_connect_four.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from util.connect_four import ConnectFour
+from connect_four import ConnectFour
 
 # Test board initialization
 def test_board_initialization():
@@ -10,13 +10,13 @@ def test_board_initialization():
     assert (game.player_two_board == np.zeros((6,7))).all()
 
 # Test state detection (win via horizontal, vertical, and diagonals)
-@pytest.skip("Awaiting Implementation")
-@pytest.mark.parametrize("board, expected_res", [[]])
+@pytest.mark.skip("Awaiting Implementation")
+@pytest.mark.parametrize("board, expected_res", [[None, None]])
 def test_state_detection(board, expected_res):
     pass
 
 # Test move selection (Legal and illegal)
-@pytest.skip("Awaiting Implementation")
-@pytest.mark.parametrize("board, move, expected_cond", [[]])
+@pytest.mark.skip("Awaiting Implementation")
+@pytest.mark.parametrize("board, move, expected_res", [[None, None, None]])
 def test_move_selection(board, move, expected_res):
     pass
