029dc2ea5eaefd47059dfcabbeee115faf1ac8d2
Richard Yang <richard.jr.yang@gmail.com>
2022-12-04 02:45:20 -0500

    adding unit tests that need handling

--- utils/tests/test_connect_four.py
+++ utils/tests/test_connect_four.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from connect_four import ConnectFour
+from util.connect_four import ConnectFour
 
 # Test board initialization
 def test_board_initialization():
@@ -9,7 +9,14 @@ def test_board_initialization():
     assert (game.player_one_board == np.zeros((6,7))).all()
     assert (game.player_two_board == np.zeros((6,7))).all()
 
-
-# Test state detection (win, lose, draw)
+# Test state detection (win via horizontal, vertical, and diagonals)
+@pytest.skip("Awaiting Implementation")
+@pytest.mark.parametrize("board, expected_res", [[]])
+def test_state_detection(board, expected_res):
+    pass
 
 # Test move selection (Legal and illegal)
+@pytest.skip("Awaiting Implementation")
+@pytest.mark.parametrize("board, move, expected_cond", [[]])
+def test_move_selection(board, move, expected_res):
+    pass
